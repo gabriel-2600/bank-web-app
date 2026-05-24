a5c46d00bbb62c5dd01085ed6baef943dc028994
Gabriel Molina <130275197+gabriel-2600@users.noreply.github.com>
2026-05-24 16:04:23 +0800

    added bcrypt password encoder

--- backend/src/main/java/com/bank/backend/service/UsersService.java
+++ backend/src/main/java/com/bank/backend/service/UsersService.java
@@ -1,19 +1,24 @@
 package com.bank.backend.service;
 
+import com.bank.backend.config.SecurityConfig;
 import com.bank.backend.entity.Users;
 import com.bank.backend.repository.UsersRepoInterface;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UsersService {
 
     private final UsersRepoInterface usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UsersService(UsersRepoInterface usersRepository){
+    public UsersService(UsersRepoInterface usersRepository, PasswordEncoder passwordEncoder){
         this.usersRepository = usersRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public void createUser(Users user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         usersRepository.save(user);
     }
 
