b13d6dce4636852f8b676054c1708fb727f35c8c
Gabriel Molina <130275197+gabriel-2600@users.noreply.github.com>
2026-07-24 22:46:17 +0800

    added clientApi request - retry - protected requests

--- frontend/src/api/clientApi.ts
+++ frontend/src/api/clientApi.ts
@@ -1,27 +1,14 @@
 import { throwError } from "../util/throw-error";
 
-interface refreshResponse {
+type HttpMethods = "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
+type BodyType = object | undefined;
+interface RefreshResponse {
   accessToken: string;
 }
 
-let accessToken: string | null;
-let callbackCopy: ((token: string | null) => void) | null = null;
 const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-export const getApi = async (endPoint: string) => {
-  const response = await fetch(`${BASE_URL}/api${endPoint}`, {
-    mode: "cors",
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-    },
-    credentials: "include",
-  });
-
-  if (response.status === 401) {
-    refreshApi();
-  }
-};
+let ACCESS_TOKEN: string | null;
+let callbackCopy: ((token: string | null) => void) | null = null;
 
 const refreshApi = async () => {
   const response = await fetch(`${BASE_URL}/api/auth/refresh`, {
@@ -35,30 +22,30 @@ const refreshApi = async () => {
 
   if (!response.ok) {
     setClientAccessToken(null);
-    notifySetAccessToken(null);
+    notifySetState(null);
 
     await throwError(response);
   }
 
-  const data: refreshResponse = await response.json();
+  const data: RefreshResponse = await response.json();
 
   console.log(data);
   setClientAccessToken(data.accessToken);
-  notifySetAccessToken(data.accessToken);
+  notifySetState(data.accessToken);
 };
 
 export const setClientAccessToken = (token: string | null) => {
-  accessToken = token;
-  console.log("ACCESS TOKEN IS SET IN TYPESCRIPT: " + accessToken);
+  ACCESS_TOKEN = token;
+  console.log("ACCESS TOKEN IS SET IN TYPESCRIPT: " + ACCESS_TOKEN);
 };
 
-export const getClientAccessT0ken = () => accessToken;
+export const getClientAccessT0ken = () => ACCESS_TOKEN;
 
 export const registerCallback = (callback: (token: string | null) => void) => {
   callbackCopy = callback;
 };
 
-function notifySetAccessToken(update: string | null) {
+function notifySetState(update: string | null) {
   if (!callbackCopy) {
     console.error("Call back is null");
     throw new Error("Call back is null");
@@ -66,3 +53,53 @@ function notifySetAccessToken(update: string | null) {
 
   callbackCopy(update);
 }
+
+export const sendRequest = async (
+  method: HttpMethods,
+  endPoint: string,
+  body: BodyType = undefined,
+  retried = false,
+) => {
+  let response;
+
+  if (body === undefined) {
+    response = await fetch(`${BASE_URL}/api${endPoint}`, {
+      mode: "cors",
+      method: `${method}`,
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+      },
+      credentials: "include",
+    });
+  } else {
+    response = await fetch(`${BASE_URL}/api${endPoint}`, {
+      mode: "cors",
+      method: `${method}`,
+      headers: {
+        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+      credentials: "include",
+    });
+  }
+
+  if (response.status === 401) {
+    if (!retried) {
+      await refreshApi();
+      return sendRequest(method, endPoint, body, true);
+    } else {
+      setClientAccessToken(null);
+      notifySetState(null);
+
+      throwError(response);
+    }
+  } else if (!response.ok) {
+    throwError(response);
+  }
+
+  const data = await response.json();
+  console.log(data);
+
+  return data;
+};
